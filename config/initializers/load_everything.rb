30745f10e83784c3bbbc1c45b34fd4caad7d0c50
Darren Cauthon <darren@cauthon.com>
2015-01-08 14:03:02 -0600

    Again, got rid of GEM_NAME.

--- config/initializers/load_everything.rb
+++ config/initializers/load_everything.rb
@@ -1,5 +1,5 @@
 WakeUpRails.load_all_ruby_files_in "#{`bundle show willow`.strip}/app"
 WakeUpRails.load_all_ruby_files_in File.dirname(__FILE__) + '/../../app'
-if ENV['CLIENT_SPECIFIC_GEM_NAME']
-  WakeUpRails.load_all_ruby_files_in "#{`bundle show #{ENV['CLIENT_SPECIFIC_GEM_NAME']}`.strip}/app"
+if ENV['CLIENT_SPECIFIC_GEM_REPO']
+  WakeUpRails.load_all_ruby_files_in "#{`bundle show clientspecific`.strip}/app"
 end
