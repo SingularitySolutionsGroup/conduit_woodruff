df4df734edbd0841b1fa5d67603b7c42747abe36
Darren Cauthon <darren@cauthon.com>
2015-04-23 13:54:51 -0500

    With tests, check for duplicate lead ids.

--- config/initializers/willow_decorators.rb
+++ config/initializers/willow_decorators.rb
@@ -2,6 +2,7 @@ WillowDecorators.setup
 
 Interchangeable.define(StudentManagementController, :a_reason_the_new_user_is_invalid) do |c, p|
   lead_id = p[:lead_id] || p[:hive_lead_id]
+  return nil unless lead_id.present?
   Refinery::User.where(hive_lead_id: lead_id).any? ?
     'A user exists with this lead id.' : nil
 end
