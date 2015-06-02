7b434c183331e5a6106c65a869fac470f6b57569
Darren Cauthon <darren@cauthon.com>
2015-06-02 16:47:47 -0500

    Hacking.

---
+++ app/model/google_recurrence.rb
@@ -0,0 +1,31 @@
+module GoogleRecurrence
+
+  def self.testing
+    event_start_date = DateTime.parse('2015-04-20 15:00:00 +0000')
+    recurrence = 'FREQ=WEEKLY;INTERVAL=1;BYDAY=MO'
+    from = 'Sun, 31 May 2015 20:43:58 +0000'
+    to   = 'Tue, 02 Jun 2016 20:43:58 +0000'
+    get_the_recurrences event_start_date, recurrence, from, to
+  end
+
+  def self.testing2
+    testing.sub('[', '').reverse.sub(']', '').reverse.split(',').map { |x| x.strip }.map { |x| DateTime.parse x }
+  end
+
+  def self.get_the_recurrences event_start_date, recurrence, from, to
+
+    statement = <<EOF
+var RRule = require('#{Rails.root}/lib/recurrence/rrule.js').RRule;
+var options = RRule.parseString('#{recurrence}');
+options.dtstart = new Date(Date.parse('#{event_start_date}'));
+var rule = new RRule(options);
+var results = rule.between(new Date(Date.parse('#{from}')), new Date(Date.parse('#{to}')));
+console.log(results);
+EOF
+
+    statement = "node -e \"#{statement}\""
+
+    %x( #{statement} )
+
+  end
+end
--- lib/recurrence/blah.rb
+++ lib/recurrence/blah.rb
@@ -1,9 +1,14 @@
+event_start_date = DateTime.parse('2015-04-20 15:00:00 +0000')
+recurrence = 'FREQ=WEEKLY;INTERVAL=1;BYDAY=MO'
+from = 'Sun, 31 May 2015 20:43:58 +0000'
+to   = 'Tue, 02 Jun 2015 20:43:58 +0000'
+
 statement = <<EOF
 var RRule = require('./rrule').RRule;
-var options = RRule.parseString('FREQ=WEEKLY;INTERVAL=1;BYDAY=MO');
-options.dtstart = new Date(Date.parse('2015-04-20 15:00:00 +0000'));
+var options = RRule.parseString('#{recurrence}');
+options.dtstart = new Date(Date.parse('#{event_start_time}'));
 var rule = new RRule(options);
-var results = rule.between(new Date(Date.parse('Sun, 31 May 2015 20:43:58 +0000')), new Date(Date.parse('Tue, 02 Jun 2015 20:43:58 +0000')));
+var results = rule.between(new Date(Date.parse('#{from}')), new Date(Date.parse('#{to}')));
 console.log(results);
 EOF
 
