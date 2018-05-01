a719fcd79755fb2012ff81ea60b741fc05fa08e4
Josh McDonald <joshkmcdonald@gmail.com>
2018-05-01 13:59:51 -0600

    Update client configuration methods

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.school_name
-      'Texas Barber College'
+      'Digital Film Academy'
     end
 
     def self.assets_directory
@@ -22,7 +22,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.email_prefix
-      'texas-barber-college-student-portal'
+      'digital-film-academy-student-portal'
     end
   end
 end
