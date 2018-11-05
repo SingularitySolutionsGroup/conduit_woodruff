0b43360da263a4080f6e84fd750082c5fa6294d3
Ferris Lucas <ferris.lucas@gmail.com>
2018-11-04 21:09:44 -0600

    run this to setup a new system

---
+++ setup_for_local_development.sh
@@ -0,0 +1,17 @@
+#/bin/bash -e
+
+echo "Bringing up postgres..."
+docker-compose up -d db
+sleep 20
+echo "Creating a new database..."
+docker-compose run db createdb -U postgres -h db --no-password conduit
+
+echo "Running rails migrations..."
+docker-compose run web rake db:migrate
+
+echo "Performing basic housekeeping for a new ADM system..."
+docker-compose run web rails runner "SystemBootstrap.basic_setup"
+
+docker-compose up -d
+echo "The system is read. Please run this to setup a root user:"
+echo "SystemBootstrap.create_root_user 'YOUR FIRST NAME', 'YOUR LAST NAME', 'YOUR EMAIL'"
