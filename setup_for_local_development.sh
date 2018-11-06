38d77d49bf8170e2248e0f6bddd8c1310a9e72f8
Ferris Lucas <ferris.lucas@gmail.com>
2018-11-05 20:39:59 -0600

    database.yml is ignored by source control

--- setup_for_local_development.sh
+++ setup_for_local_development.sh
@@ -7,6 +7,7 @@ echo "Creating a new database..."
 docker-compose run db createdb -U postgres -h db --no-password conduit
 
 echo "Running rails migrations..."
+cp config/database.yml.compose config/database.yml
 docker-compose run web rake db:migrate
 
 echo "Performing basic housekeeping for a new ADM system..."
@@ -14,4 +15,4 @@ docker-compose run web rails runner "SystemBootstrap.basic_setup"
 
 docker-compose up -d
 echo "The system is ready. Please run this to setup a root user:"
-echo "SystemBootstrap.create_root_user 'YOUR FIRST NAME', 'YOUR LAST NAME', 'YOUR EMAIL'"
+echo "docker-compose run web rails runner \"SystemBootstrap.create_root_user 'YOUR FIRST NAME', 'YOUR LAST NAME', 'YOUR EMAIL'\""
