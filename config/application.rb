c84c956da84662a3d9e2ce4ac02338e26b3644ed
VajkFreyasman <craig.linde.com>
2020-01-24 14:39:12 -0700

    found one more ref to new charter

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.school_name
-      'New Charter'
+      'Eastern Virginia Career College'
     end
 
     def self.assets_directory
@@ -22,7 +22,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.email_prefix
-      'new-charter-student-portal'
+      'evcc-student-portal'
     end
   end
 end
