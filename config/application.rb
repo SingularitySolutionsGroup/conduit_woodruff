bd02f4d19685d798374eb6bb2b5897efef25200a
Siddheshwar Lomte <sidhesh0311@gmail.com>
2023-06-20 15:20:06 +0530

    DEVELOP: Logo, favicon & social media link changes

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.school_name
-      'Woodruff Medical'
+      'Genesis'
     end
 
     def self.assets_directory
@@ -22,7 +22,7 @@ unless Object.const_defined?('ClientConfiguration')
     end
 
     def self.email_prefix
-      'woodruff-student-portal'
+      'Genesis-student-portal'
     end
   end
 end
