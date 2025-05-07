d3d5ed4e50ce57066c6db8d60c847360fe15d860
Giovanni Delgado <giovanni.delgado@itti.digital>
2025-05-06 23:44:58 -0600

    Add configuration files

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 # FROM quay.io/ssgadmin/conduit_base:master
 FROM 089140149962.dkr.ecr.us-east-1.amazonaws.com/conduit_base:latest
-ENV BUNDLE_GITHUB__COM=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:x-oauth-basic
+# FROM techdiamondsis/conduit_base:latest
+ENV BUNDLE_GITHUB__COM=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:x-oauth-basic
 RUN mkdir /home/app/phantomjs-2.1.1-linux-x86_64
 COPY ./phantomjs-2.1.1-linux-x86_64 /home/app/phantomjs-2.1.1-linux-x86_64/.
 RUN ls /home/app/
 RUN ln -sf /home/app/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 ADD ./ /home/app/webapp
-RUN bundle install --jobs 10
+RUN bundle install --jobs 16
--- setup_for_local_development.sh
+++ setup_for_local_development.sh
@@ -1,18 +1,18 @@
 #/bin/bash -e
 
 echo "Bringing up postgres..."
-docker-compose up -d db
+docker compose up -d db
 sleep 20
 echo "Creating a new database..."
-docker-compose run db createdb -U postgres -h db --no-password conduit
+docker compose run db createdb -U postgres -h db --no-password conduit
 
 echo "Running rails migrations..."
 cp config/database.yml.compose config/database.yml
-docker-compose run web rake db:migrate
+docker compose run web rake db:migrate
 
 echo "Performing basic housekeeping for a new ADM system..."
-docker-compose run web rails runner "SystemBootstrap.basic_setup"
+docker compose run web rails runner "SystemBootstrap.basic_setup"
 
-docker-compose up -d
+docker compose up -d
 echo "The system is ready. Please run this to setup a root user:"
-echo "docker-compose run web rails runner \"SystemBootstrap.create_root_user 'YOUR FIRST NAME', 'YOUR LAST NAME', 'YOUR EMAIL'\""
+echo "docker compose run web rails runner \"SystemBootstrap.create_root_user 'YOUR FIRST NAME', 'YOUR LAST NAME', 'YOUR EMAIL'\""
